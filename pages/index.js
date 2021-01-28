04cba41dad829c0414a8b322f31c85072a8ccd24
HugoPoletto34 <hugopoletto34@gmail.com>
2021-01-27 22:38:00 -0300

    Aula 2 - Linter, Rotas e State

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import React from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -16,15 +20,10 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
-const BackgrondImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -33,19 +32,36 @@ export default function Home() {
             <h1>Death Note</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem ipsum dolor sit amet</p>
+            <p>Informe seu nome</p> 
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+                placeholder="(prometo não escrever no Death Note "
+                value={name}
+                name="nomeDoUsuario"
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
           <Widget.Content>
-            <h1>Death Note</h1>
+            <h1>Quizes da Galera</h1>
             <p>lorem ipsum dolor sit amet</p>
           </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/HugoPoletto34/"/>
+      <GitHubCorner projectUrl="https://github.com/HugoPoletto34/" />
     </QuizBackground>
   );
 }
---
+++ pages/quiz.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import db from '../db';
+
+export default function QuizPage() {
+  return (
+    <div>
+      Pàginaasdasdasdsd
+    </div>
+  );
+}
