3979d8266c41ad88b41b9dbe7115e03079a90503
HugoPoletto34 <hugopoletto34@gmail.com>
2021-01-28 14:41:59 -0300

    Aula 3 - Criando Quiz e API

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import styled from 'styled-components'
 
 const Title = styled.h1`
@@ -7,4 +8,63 @@ const Title = styled.h1`
 
 export default function Home() {
   return <Title>My page</Title>
+=======
+import React from 'react';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
+
+export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Death Note</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Informe seu nome</p>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+                placeholder="(prometo não escrever no Death Note)"
+                value={name}
+                name="nomeDoUsuario"
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>lorem ipsum dolor sit amet</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/HugoPoletto34/Death-Note-Quiz" />
+    </QuizBackground>
+  );
+>>>>>>> Stashed changes
 }
