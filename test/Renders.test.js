659fb443fac81e217cb790b818fe66394c2df349
Hugo Poletto A. Gomes <67022441+HugoPoletto34@users.noreply.github.com>
2023-10-16 23:57:09 +0000

    teste verificar input

--- test/Renders.test.js
+++ test/Renders.test.js
@@ -4,6 +4,10 @@ import Input from '../src/components/Input';
 import Button from '../src/components/Button';
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Link from '../src/components/Link';
+
 
 describe("Render Footer", () => {
   it("Verificar se o footer personalizado tem a tag footer", () => {
@@ -15,3 +19,31 @@ describe("Render Footer", () => {
   });
 });
 
+describe("Render Formulario", () => {
+  it("Verificar se o input está como 'Jogar Hugo'", () => {
+    const name = "Hugo";
+    const { getByText } = render(
+      <Formulario
+        onSubmit={(event) => {
+          event.preventDefault();
+          router.push(`/quiz?name=${name}`);
+        }}
+      >
+        <input
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+          placeholder="(prometo não escrever no Death Note)"
+          value={name}
+          name="nomeDoUsuario"
+        />
+        <button type="submit" disabled={name.length === 0}>
+          {`Jogar ${name}`}
+        </button>
+      </Formulario>
+    );
+  
+    const botao = getByText(`Jogar ${name}`);
+    expect(botao).toBeInTheDocument();
+  });
+});
