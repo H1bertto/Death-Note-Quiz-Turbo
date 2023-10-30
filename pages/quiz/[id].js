ffb45526eca2548703cd32852891820d10d78651
Hugo Poletto A. Gomes <67022441+HugoPoletto34@users.noreply.github.com>
2023-10-30 20:23:01 +0000

    Mudanças removendo getServerSideProps

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -3,33 +3,46 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import QuizScreen from '../../src/screens/Quiz';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { headers } from '../../next.config';
 
-export default function QuizDaGalera({ dbExterno }) {
-  return (
-    <ThemeProvider theme={dbExterno.theme}>
-      <QuizScreen
-        externalQuestions={dbExterno.questions}
-        externalBg={dbExterno.bg}
-      />
-    </ThemeProvider>
-  );
-}
+export default function QuizDaGalera() {
+  const router = useRouter();
+  const [dbExterno, setDbExterno] = React.useState(null);
 
-export async function getServerSideProps(context) {
-  const [repName, gitHubUser] = context.query.id.split('___');
-  try {
-    const dbExterno = await fetch(`https://${repName}.${gitHubUser}.vercel.app/api/db`)
-      .then((respServer) => {
-        if (respServer.ok) {
-          return respServer.json();
-        }
-        throw new Error('Falhou em pegar dados');
-      })
-      .then((respInObject) => respInObject);
-    return {
-      props: { dbExterno },
-    };
-  } catch (err) {
-    throw new Error(err);
+  if (router.query.id) {
+    const [repName, gitHubUser] = router.query.id.split('___');
+    load(repName, gitHubUser);
+  } else {
+    return <div>Carregando...</div>;
   }
+
+
+
+  function load(repName, gitHubUser) {
+    console.log(repName, gitHubUser)
+
+    fetch(`https://${repName}.${gitHubUser}.vercel.app/api/db`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://127.0.0.1:3000',
+        'Access-Control-Allow-Methods': 'GET, POST'
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setDbExterno(response);
+      });
+  }
+  ;
+
+  // return (
+  //   <ThemeProvider theme={dbExterno.theme}>
+  //     <QuizScreen
+  //       externalQuestions={dbExterno.questions}
+  //       externalBg={dbExterno.bg}
+  //     />
+  //   </ThemeProvider>
+  // );
 }
